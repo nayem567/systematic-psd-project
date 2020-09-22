045c6343db885c5633d38c83e344a078edbfd851
nayem567 <66139173+nayem567@users.noreply.github.com>
2020-09-22 19:18:01 +0600

    Add files via upload

---
+++ vendors/js/main.js
@@ -0,0 +1,14 @@
+
+
+//  BX Slider
+
+$(document).ready(function(){
+    $('.slider-area').bxSlider();
+    jQuery('#main-menu').meanmenu({
+      meanMenuContainer: '#mobile-menu',
+      meanScreenWidth:991,
+    });
+
+  });
+
+// 
